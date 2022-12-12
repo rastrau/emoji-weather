64c58456b2ff2f8e85b8ef44b13bcc324598c275
rastrau <Ralph.Straumann@ebp.ch>
2022-12-12 17:32:46 +0100

    Update reprex.py

--- reprex.py
+++ reprex.py
@@ -6,9 +6,9 @@ from mastodon import Mastodon
     
 if __name__ == "__main__":
    
-    MASTODON_ACCESS_TOKEN = os.environ.get("MASTODON_ACCESS_TOKEN")
+    MASTO_TEST_TOKEN = os.environ.get("MASTO_TEST_TOKEN")
     MASTODON_URL = "https://tooting.ch"
-    print(type(MASTODON_ACCESS_TOKEN))
-    print(MASTODON_ACCESS_TOKEN)
-    masto = Mastodon(api_base_url = MASTODON_URL, access_token = MASTODON_ACCESS_TOKEN)
+    print(type(MASTO_TEST_TOKEN))
+    print(MASTO_TEST_TOKEN)
+    masto = Mastodon(api_base_url = MASTODON_URL, access_token = MASTO_TEST_TOKEN)
     masto.toot("This is a test!")
